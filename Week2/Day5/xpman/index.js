5a57e3ab70a9becebed7886ccd171a09311db26f
sebo964 <Sebastien.arokeum@gmail.com>
2023-01-29 21:26:54 +0400

    day5w2

--- Week2/Day5/xpman/index.js
+++ Week2/Day5/xpman/index.js
@@ -9,7 +9,6 @@
 // Take a look at your html file, you should have a button with an “onclick” event. This event is equal to the function playTheGame(). It means that when you will click on the button, the function playTheGame() will be called. 
 // We will learn more about events next week ;)
 
-
 // Now let’s create the function:
 
 // In the JS file, create a function called playTheGame() that takes no parameter.
@@ -42,3 +41,96 @@
 // Bonus
 
 // In the First Part, instead of stopping the process if the user didn’t enter a valid number. Continue asking for a number until the user enters a valid number.
+
+
+function playTheGame() {
+
+    confirm ('Do you want to play the game?') 
+
+            // generate a random number from 1 to 10 
+            let computerNumber = parseInt(Math.floor(Math.random() * 10 + 0))
+            console.log(computerNumber)
+
+        
+            let userNumber= parseInt(prompt('Enter a number that is between 0 and 10'))
+
+
+            // check it is a number
+            numbercheck(userNumber) 
+            console.log(numbercheck(userNumber))
+            // check if it is a valid number
+            isNumbervalid(userNumber)  
+            console.log(isNumbervalid(userNumber))
+
+    // if it is a number and the number is valid proceed otherwise eend the game
+    if (numbercheck(userNumber) && isNumbervalid(userNumber) == true) {
+
+        // check if the number is a winner if not propose the user another 3 tries to get it right
+        for (let i = 0; i < 3; i++) {  
+
+            
+            if (computerNumber === userNumber) {
+                console.log('win')
+                return (alert ('You win'))
+                }
+            else if (computerNumber > userNumber) {
+                alert('lose small')
+
+            }
+            else if (computerNumber < userNumber) {
+                alert('lose big')
+
+                }
+             userNumber=  parseInt(prompt('try again'))
+
+        }
+
+           return (alert("Game over!"))
+}
+else {
+    return (alert('out of chances'))
+    
+}    
+        
+}       
+
+
+
+// check if the userNumber is a number
+function numbercheck(quserNumber){
+    for(let i=0; i<1; i++) {
+        if ( isNaN(quserNumber)) {
+            alert('This is not a number. Please enter a number')
+            quserNumber= parseInt (prompt('Please enter a number'))
+            
+        }
+        else {
+            
+            return true
+            break
+            
+        }
+    }
+   return( alert('You had only 3 tries'))
+    
+
+}
+
+// check is the number is between 1 to 10 
+function isNumbervalid(userNumber){
+    for (i=0;i<1;i++) {
+
+        if (userNumber > 10 || userNumber < 0) {
+            alert('Sorry number not valid')
+            userNumber= parseInt (prompt('Please enter a number between 0 and 10'))
+        }   
+        else {
+            return true
+            break
+        }
+    }
+  return(alert('too many tries'))
+    
+}
+
+
