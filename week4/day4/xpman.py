644982ddcaed06ee2ebe8bbf308e723860e43e01
sebo964 <Sebastien.arokeum@gmail.com>
2023-02-09 20:17:15 +0400

    py4

--- week4/day4/xpman.py
+++ week4/day4/xpman.py
@@ -6,6 +6,13 @@
 # Call the function, and make sure the message displays correctly.
 
 
+def display_message():
+    print("I'm learning python")
+
+
+display_message()
+
+
 # 🌟 Exercise 2: What’s Your Favorite Book ?
 
 # Instructions
@@ -14,6 +21,13 @@
 # The function should print a message, such as "One of my favorite books is <title>".
 # For example: “One of my favorite books is Alice in Wonderland”
 # Call the function, make sure to include a book title as an argument when calling the function.
+
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}")
+
+
+favorite_book("Fellow ship of the ring")
 
 
 # 🌟 Exercise 3 : Some Geography
@@ -27,6 +41,15 @@
 # Call your function.
 
 
+def describe_city(city="Rose Hill", country="Mauritius"):
+    print(f"{city.title()} is in {country.title()} and it's a great place")
+
+
+describe_city("new york", "usa")
+
+describe_city()
+
+
 # Exercise 4 : Random
 
 # Instructions
@@ -34,6 +57,22 @@
 # Create a function that accepts a number between 1 and 100 and generates another number randomly between 1 and 100.
 # Compare the two numbers, if it’s the same number, display a success message, otherwise show a fail message and display both numbers.
 
+
+def checknum(num):
+    import random
+
+    random_number = random.randint(1, 100)
+
+    if num > 100 or num < 1:
+        print("ERROR, parameter not between 1 and 100")
+
+    if num == random_number:
+        print("You guessed the number")
+    else:
+        print(f"You didn't guess the number. The number was {random_number}")
+
+
+checknum(12)
 
 # 🌟 Exercise 5 : Let’s Create Some Personalized Shirts !
 
@@ -51,6 +90,23 @@
 # Bonus: Call the function make_shirt() using keyword arguments.
 
 
+def make_shirt(txt="I love Python", size="Large"):
+    print(f"The size of the shirt is {size} and the text is {txt}")
+
+
+# Make a large shirt with the default message
+
+make_shirt()
+
+# Make medium shirt with the default message
+
+make_shirt(size="medium")
+
+# Make a shirt of any size with a different message.
+
+make_shirt(txt="I hate java")
+
+
 # 🌟 Exercise 6 : Magicians …
 
 # Instructions
@@ -60,6 +116,29 @@
 # Write a function called make_great() that modifies the list of magicians by adding the phrase "the Great" to each magician’s name.
 # Call the function make_great().
 # Call the function show_magicians() to see that the list has actually been modified.
+
+magician_names = ["Harry Houdini", "David Blaine", "Criss Angel"]
+
+
+def show_magicians(**args):
+    for args in magician_names:
+        print(args)
+
+
+show_magicians()
+
+# Write a function called make_great() that modifies the list of magicians by adding the phrase "the Great" to each magician’s name.
+
+
+def make_great(**args):
+    for args in magician_names:
+        index_of_aargs = magician_names.index(args)
+        magician_names.pop(index_of_aargs)
+        magician_names.insert(index_of_aargs, f"{args} the Great")
+
+
+make_great()
+show_magicians()
 
 
 # 🌟 Exercise 7 : Temperature Advice
@@ -90,3 +169,38 @@
 
 # Bonus: Give the temperature as a floating-point number instead of an integer.
 # Bonus: Instead of asking for the season, ask the user for the number of the month (1 = January, 12 = December). Determine the season according to the month.
+
+
+user_season = input("Press enter to season: ")
+
+
+def get_random_temp(season):
+    import random
+
+    if season == "winter":
+        return random.randint(-10, 16)
+    if season == "summer":
+        return random.randint(24, 40)
+    if season == "spring":
+        return random.randint(16, 23)
+    if season == "fall":
+        return random.randint(16, 23)
+
+
+def main():
+    temp = get_random_temp(user_season)
+    print(f"The temperature right now is {temp} degrees Celsius.")
+    if temp <= 0:
+        print("Brrr, that’s freezing! Wear some extra layers today")
+    elif temp >= 0 and temp < 16:
+        print("Quite chilly! Don’t forget your coat")
+    elif temp >= 16 and temp <= 23:
+        print("It's a nice day")
+
+    elif temp >= 24 and temp <= 32:
+        print("It's a hot day")
+    else:
+        print("It's a very hot day")
+
+
+main()
