3562d657b4b2d031971e8063bc73d09228897e08
sebo964 <sebastien.arokeum@gmail.com>
2023-03-11 20:21:38 +0400

    week5day2

--- week5/day2/dailychal.py
+++ week5/day2/dailychal.py
@@ -66,144 +66,67 @@ Pagination_size = 4
 the_current_page_is = 0
 
 
-# class Pagination:
-#     def __init__(self, alphabetlist, Pagination_size):
-#         self.alphabetlist = alphabetlist
-#         self.Pagination_size = Pagination_size
-#         self.totalPages = math.ceil(len(alphabetlist) / Pagination_size) - 1
-#         self.currentPage = 0
-
-#     def getVisibleItems(self):
-#         return self.alphabetlist[
-#             self.Pagination_size
-#             * self.currentPage : (
-#                 self.Pagination_size * (self.currentPage) + self.Pagination_size
-#             )
-#         ]
-
-#     def nextPage(self):
-#         if self.currentPage > self.totalPages - 1:
-#             return print("You are on the last page")
-#         else:
-#             self.currentPage += 1
-#             return self.getVisibleItems()
-
-#     def prevPage(self):
-#         if self.currentPage <= 1:
-#             return print("You are on the first page")
-#         else:
-#             self.currentPage -= 1
-#             print(self.currentPage)
-#             print(self.totalPages)
-#             return self.alphabetlist[
-#                 self.Pagination_size
-#                 * self.currentPage : (
-#                     self.Pagination_size * (self.currentPage) + self.Pagination_size
-#                 )
-#             ]
-
-#     def firstPage(self):
-#         self.currentPage = 0
-#         print(self.currentPage)
-#         self.getVisibleItems()
-
-#     def lastPage(self):
-#         self.currentPage = self.totalPages
-#         self.getVisibleItems()
-
-#     def goToPage(self, pageNum):
-#         self.currentPage = pageNum
-#         return self.alphabetlist[
-#             self.Pagination_size
-#             * self.currentPage : (
-#                 self.Pagination_size * (self.currentPage) + self.Pagination_size
-#             )
-#         ]
-
-
-# page1 = Pagination(alphabetlist, Pagination_size)
-
-
-# print(page1.getVisibleItems())
-
-# print(page1.nextPage())
-
-# print(page1.nextPage())
-# print(page1.nextPage())
-# print(page1.nextPage())
-# print(page1.nextPage())
-# print(page1.nextPage())
-# print(page1.nextPage())
-# print(page1.nextPage())
-
-
-# print(page1.goToPage(0))
-
-
 class Pagination:
-    def __init__(self, items=[], pageSize=2):
-        self.items = items
-        self.pageSize = int(pageSize)
-        self.currentPage = 1
-        self.totalPages = (
-            1 if len(items) == 0 else (len(items) - 1) // self.pageSize + 1
-        )
+    def __init__(self, alphabetlist, Pagination_size):
+        self.alphabetlist = alphabetlist
+        self.Pagination_size = Pagination_size
+        self.totalPages = math.ceil(len(alphabetlist) / Pagination_size) - 1
+        self.currentPage = 0
 
     def getVisibleItems(self):
-        start = (self.currentPage - 1) * self.pageSize
-        end = start + self.pageSize
-        return self.items[start:end]
-
-    def prevPage(self):
-        self.currentPage = max(self.currentPage - 1, 1)
-        return self
+        return self.alphabetlist[
+            self.Pagination_size
+            * self.currentPage : (
+                self.Pagination_size * (self.currentPage) + self.Pagination_size
+            )
+        ]
 
     def nextPage(self):
-        self.currentPage = min(self.currentPage + 1, self.totalPages)
-        return self
+        if self.currentPage > self.totalPages - 1:
+            return print("You are on the last page")
+        else:
+            self.currentPage += 1
+            return self.getVisibleItems()
+
+    def prevPage(self):
+        if self.currentPage <= 1:
+            return print("You are on the first page")
+        else:
+            self.currentPage -= 1
+            print(self.getVisibleItems())
 
     def firstPage(self):
-        self.currentPage = 1
-        return self
+        self.currentPage = 0
+        print(self.currentPage)
+        self.getVisibleItems()
 
     def lastPage(self):
         self.currentPage = self.totalPages
-        return self
+        self.getVisibleItems()
 
     def goToPage(self, pageNum):
-        pageNum = int(pageNum)
-        if pageNum <= 0:
-            self.currentPage = 1
-        elif pageNum > self.totalPages:
-            self.currentPage = self.totalPages
-        else:
-            self.currentPage = pageNum
-        return self
+        self.currentPage = pageNum
+        return self.alphabetlist[
+            self.Pagination_size
+            * self.currentPage : (
+                self.Pagination_size * (self.currentPage) + self.Pagination_size
+            )
+        ]
 
 
-page = Pagination(alphabetlist, Pagination_size)
-print(page.getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
-print(page.nextPage().getVisibleItems())
+page1 = Pagination(alphabetlist, Pagination_size)
 
 
-print(page.goToPage(8).getVisibleItems())
+print(page1.getVisibleItems())
+print(page1.nextPage())
+print(page1.nextPage())
+print(page1.nextPage())
+print(page1.nextPage())
+print(page1.prevPage())
+print(page1.nextPage())
+print(page1.nextPage())
+print(page1.nextPage())
+print(page1.nextPage())
 
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
-print(page.prevPage().getVisibleItems())
 
-print(page.firstPage().getVisibleItems())
-print(page.lastPage().getVisibleItems())
+print(page1.goToPage(1))
