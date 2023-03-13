34d8b1716c622b8239dc93ed9b5cd9a2c69cd871
sebo964 <sebastien.arokeum@gmail.com>
2023-03-13 22:43:51 +0400

    wk9

--- week9/day1/xpman/FilmProject/films/urls.py
+++ week9/day1/xpman/FilmProject/films/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("film/addfilm", views.addFilm, name="addFilm"),
-    path("director/addfirector", views.addDirector, name="addDirector"),
+    path("director/adddirector", views.addDirector, name="addDirector"),
 ]
